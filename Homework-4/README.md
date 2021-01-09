025fa8380e191a29c8549e8c3a680178d9791e90
Justin Vang <75344606+justinkvang@users.noreply.github.com>
2021-01-08 21:24:29 -0600

    Add files via upload
    Uploading homework 4

---
+++ Homework-4/README.md
@@ -0,0 +1,31 @@
+# Code Quiz
+What:
+A 60 second quiz.
+
+Rules:
+You will only have 60 seconds to complete the quiz. 
+Once you select your answer, you can't reselect. 
+If you answer incorrectly, 10 seconds will be subtracted from your current time. 
+Your score is based on how much time is lefted over.
+
+## What I Did
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+
+![code quiz](./Assets/HW-4.gif)
+# Justin-s-Code-Quiz
+# My-Code-Quiz
+# My-Code-Quiz
+# My-Code-Quiz
+# My-Code-Quiz
+# My-Code-Quiz
+# My-Code-Quiz
---
+++ Homework-4/questions.js
@@ -0,0 +1,55 @@
+//Question List
+var questionList = [
+    {
+    title: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+        "Hyper Text Preprocessor",
+        "Hyper Text Markup Language",
+        "Hyper Text Multiple Language",
+        "Hyper Tool Multi Language"
+    ]
+    },
+    {
+    title: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    title: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    title: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    title: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
+
+
